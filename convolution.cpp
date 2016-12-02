d62bdf8a618b3b49cf15485debb05ac3aebf9a5b
Marqueza <alemarquez.he@gmail.com>
2016-12-01 19:38:33 -0500

    first commit

--- convolution.cpp
+++ convolution.cpp
@@ -77,26 +77,6 @@ int main()
 	//smooth.display();
 	//img.display();
 	
-	int nthreads, tid;
-
-/* Fork a team of threads giving them their own copies of variables */
-#pragma omp parallel private(nthreads, tid)
-  {
-
-  /* Obtain thread number */
-  tid = omp_get_thread_num();
-  printf("Hello World from thread = %d\n", tid);
-
-  /* Only master thread does this */
-  if (tid == 0) 
-    {
-    nthreads = omp_get_num_threads();
-    printf("Number of threads = %d\n", nthreads);
-    }
-
-  }  /* All threads join master thread and disband */
-
-	
 	return 0;
 	
 }
